83e96d51bde131b74968700d473819ae31987a39
hanx_smile <hanx_smile@icloud.com>
2023-05-09 21:44:50 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-srun -p bigdata --gres=gpu:4 --quotatype=reserved torchrun --nproc_per_node=4 mmgpt/train/instruction_finetune.py \
+nohup srun -p bigdata --gres=gpu:4 --quotatype=reserved torchrun --nproc_per_node=4 mmgpt/train/instruction_finetune.py \
   --lm_path checkpoints/llama-7b-hf \
   --tokenizer_path checkpoints/llama-7b-hf \
   --pretrained_path checkpoints/OpenFlamingo-9B/checkpoint.pt \
   --run_name train-my-gpt4 \
   --learning_rate 1e-5 \
   --lr_scheduler cosine \
-  --batch_size 8 \
+  --batch_size 2 \
   --tuning_config configs/lora_config.py \
   --dataset_config configs/sqa_dataset_config.py \
-  --report_to_wandb
+  >> /mnt/lustre/hanxiao/input/log/flamingo_train.log  2>&1 &
